bce64ac46c31c316856708afa715ec49021406a5
xiekind <ziekind07@gmail.com>
2019-12-03 13:22:43 +0800

    Modified Footer

--- src/components/frame/Footer.vue
+++ src/components/frame/Footer.vue
@@ -1,64 +1,57 @@
 <template>
-<div class="Footer">
-  <v-footer
-    dark
-    padless
-  >
-    <v-card
-      flat
-      tile
-      class="indigo lighten-1 white--text text-center"
-    >
-      <v-card-text>
-        <v-btn
-          v-for="icon in icons"
-          :key="icon"
-          class="mx-4 white--text"
-          icon
-        >
-          <v-icon size="24px">{{ icon }}</v-icon>
-        </v-btn>
-      </v-card-text>
+  <div class="Footer">
+    <v-footer id="foot" dark padless>
+      <v-card class="indigo lighten-1 white--text text-center">
+        <v-card-text>
+          <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            class="mx-4 white--text"
+            icon
+          >
+            <v-icon size="24px">{{ icon }}</v-icon>
+          </v-btn>
+        </v-card-text>
 
-      <v-card-text class="white--text pt-0">
-        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-      </v-card-text>
+        <v-card-text class="white--text pt-0">
+          Abangbalay is your one way tool for searching your ideal boarding
+          houses for your long term stay
 
-      <v-divider></v-divider>
+          <v-divider></v-divider>
 
-      <v-card-text class="white--text">
-        <strong><img :src="require('@/assets/images/MyFrontLogo.png')"></strong><br>
-        <!-- <br>{{ new Date().getFullYear() }}  -->
-
-      </v-card-text>
-    </v-card>
-  </v-footer>
-</div>
+          <strong>
+            <img :src="require('@/assets/images/MyFrontLogo.png')" />
+          </strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+  </div>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      icons: [
-        'fab fa-facebook',
-        'fab fa-twitter',
-        'fab fa-google-plus',
-        'fab fa-linkedin',
-        'fab fa-instagram',
-      ],
-    }),
-  }
+export default {
+  data: () => ({
+    icons: [
+      "fab fa-facebook",
+      "fab fa-twitter",
+      "fab fa-google-plus",
+      "fab fa-linkedin",
+      "fab fa-instagram"
+    ]
+  })
+};
 </script>
 
 <style scoped>
-
-.Footer{
+.Footer {
   margin-top: 30% !important;
+  width: 100% !important;
 }
 
-img{
+img {
   width: 100px !important;
 }
-
+v-card-text {
+  width: 100% !important;
+}
 </style>
-
